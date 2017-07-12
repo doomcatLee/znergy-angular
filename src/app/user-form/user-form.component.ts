20b63fc103a64fa44713f9529ae6bd5a6cf167e9
znergy <ryanjonesirl@gmail.com>
2017-07-12 16:53:36 -0700

    add code to create dynamodb user

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -12,6 +12,16 @@ export class UserFormComponent {
   }
   loginUser(){
     alert("clicked login!");
+    var username = (<HTMLInputElement>document.getElementById('username')).value;
+    console.log(username);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+      }
+    };
+    xhttp.open("GET", "https://jxfmonfrfa.execute-api.us-west-2.amazonaws.com/prod/import-data?tableName=students&email=" + username + "@gmail.com&name=" + username + "&customKey1=age&customValue1=200", true);
+    xhttp.send();
   }
 
 }
