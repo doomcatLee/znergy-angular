2b482a2cfc83f2c86c8fb47a3ac02c908e8e2b65
Alex Lee <doomcat9167@gmail.com>
2017-07-20 14:37:20 -0700

    form is now added and displays the right values in the console log

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -27,6 +27,7 @@ import { GenerateUrlComponent } from './generate-url/generate-url.component';
     FormsModule,
     HttpModule,
     routing,
+    ReactiveFormsModule,
     BrowserModule,
     FormsModule,
     HttpModule,
--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
@@ -10,6 +11,21 @@ export class UserRegistrationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submitForm(username: string, password:string, passwordConfirm: string, company:string,firstName:string, lastName:string, address: string, addressTwo:string, city: string, state: string, postalCode: string ): void {
+    console.log('username', username);
+    console.log('Password', password);
+    console.log('PasswordConfirm', passwordConfirm);
+    console.log('Company', company);
+    console.log('FirstName', firstName);
+    console.log('LastName', lastName);
+    console.log('Address', address);
+    console.log('Address2', addressTwo);
+    console.log('City', city);
+    console.log('State', state);
+    console.log('PostalCode', postalCode);
+
   }
 
 }
