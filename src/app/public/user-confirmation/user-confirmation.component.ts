09a27f9bbe89bca0f8d5e85004d27fdfc74452db
Kaya Winn <mail@psyreal.net>
2017-07-21 17:12:00 -0700

    segregate components in public and secured folders

--- src/app/public/user-confirmation/user-confirmation.component.ts
+++ src/app/public/user-confirmation/user-confirmation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/operator/map';
-import {ApiService} from "../services/api.service";
+import {ApiService} from '../../services/api.service';
 
 @Component({
   selector: 'app-user-confirmation',
